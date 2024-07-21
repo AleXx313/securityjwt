4f572c3edbd3733dfa02d74fe7daa8bcc6052ef9
Alexey Mironov <alexeymironov1991@gmail.com>
2024-07-21 11:40:31 +0300

    init

---
+++ src/test/java/ru/mironov/securityjwt/SecurityjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mironov.securityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
